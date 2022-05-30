e374625b138ddd97ec8a80bc9cdc1db3064ee52e
André Alves <andr.alves@ua.pt>
2022-05-30 14:55:24 +0100

    nice(-16)

--- diffclock.sh
+++ diffclock.sh
@@ -58,7 +58,7 @@ done
 
 while (( sec > 0 ))
 do
-    nice -n -19 clockdiff -o "$source" &>> "$outfile" &
+    nice -n -16 clockdiff -o "$source" &>> "$outfile" &
     ((sec -= 1))
     sleep 1
 done
