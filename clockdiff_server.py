e32f447e8934cf74592b86a5c7cd070de5127516
André Alves <andr.alves@ua.pt>
2022-06-01 19:56:30 +0100

    close

--- clockdiff_server.py
+++ clockdiff_server.py
@@ -1,43 +1,44 @@
-#!/usr/bin/env python3                                                                                                                                                                                               
-                                                                                                                                                                                                                     
-'''                                                                                                                                                                                                                  
-Clockdiff Server                                                                                                                                                                                                     
-                                                                                                                                                                                                                     
-@author: Vitor Cunha (vitorcunha@av.it.pt)                                                                                                                                                                           
-'''                                                                                                                                                                                                                  
-                                                                                                                                                                                                                     
-import time                                                                                                                                                                                                          
-import threading                                                                                                                                                                                                     
-import socket                                                                                                                                                                                                        
-                                                                                                                                                                                                                     
-HOST = ''                 # Symbolic name meaning all available interfaces                                                                                                                                           
-PORT = 31048              # Arbitrary non-privileged port                                                                                                                                                            
-                                                                                                                                                                                                                     
-RTT_ITERS = 1000                                                                                                                                                                                                     
-                                                                                                                                                                                                                     
-def main():                                                                                                                                                                                                          
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                                                                                                                                                             
-    s.bind((HOST, PORT))                                                                                                                                                                                             
-                                                                                                                                                                                                                     
-    # Measuring RTT                                                                                                                                                                                                  
-    i = RTT_ITERS                                                                                                                                                                                                    
-    while i > 0:                                                                                                                                                                                                     
-        data, addr = s.recvfrom(4096)                                                                                                                                                                                
-        s.sendto(data, addr)                                                                                                                                                                                         
-        i = i - 1                                                                                                                                                                                                    
-                                                                                                                                                                                                                     
-    # Measuring Clock offset                                                                                                                                                                                         
-    while True:                                                                                                                                                                                                      
+#!/usr/bin/env python3
+
+'''
+Clockdiff Server
+
+@author: Vitor Cunha (vitorcunha@av.it.pt)
+'''
+
+import time
+import threading
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 31048              # Arbitrary non-privileged port
+
+RTT_ITERS = 1000
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+
+    # Measuring RTT
+    i = RTT_ITERS
+    while i > 0:
+        data, addr = s.recvfrom(4096)
+        s.sendto(data, addr)
+        i = i - 1
+
+    # Measuring Clock offset
+    while True:
         data, addr = s.recvfrom(4096)
         if (data[0] == b'S'):
+            print("close")
             s.close()
-            break        
-        local_ts     = int(time.time() * 1000000)                                                                                                                                                                    
-        remote_ts    = int.from_bytes(data, byteorder='big', signed=False)                                                                                                                                           
-        delta        = (local_ts - remote_ts)                                                                                                                                                                        
-        data         = delta.to_bytes(128 // 8, byteorder='big', signed=True)                                                                                                                                                     
-        s.sendto(data, addr)                                                                                                                                                                                         
-                                                                                                                                                                                                                     
-                                                                                                                                                                                                                     
-if __name__ == "__main__":                                                                                                                                                                                           
+            break
+        local_ts     = int(time.time() * 1000000)
+        remote_ts    = int.from_bytes(data, byteorder='big', signed=False)
+        delta        = (local_ts - remote_ts)
+        data         = delta.to_bytes(128 // 8, byteorder='big', signed=True)
+        s.sendto(data, addr)
+
+
+if __name__ == "__main__":
     main()
