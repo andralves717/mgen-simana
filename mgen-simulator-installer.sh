9be9cbe105a28bb67bdff94e261ac759e6aa6872
André Alves <andr.alves@ua.pt>
2022-06-01 15:53:24 +0100

    installer

--- mgen-simulator-installer.sh
+++ mgen-simulator-installer.sh
@@ -7,25 +7,25 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 if [[ ! ":$PATH:" == *":$INSTALL_PATH:"* ]]; then
-    touch "$HOME"/.pam_environment
     mkdir -p $INSTALL_PATH
     echo "Relog and the rerun ./mgen-simulator-install.sh"
 
 fi
-    [[ ! -d "$INSTALL_PATH" ]] && mkdir -p "$INSTALL_PATH"
 
-    chmod +x mgen-simulator.sh
-    chmod +x diffclock.sh
-    chmod +x clockdiff_client.py
-    chmod +x clockdiff_server.py
-    chmod +x analyze_latency_jitter_mgen.awk
-    chmod +x analyze_latency_jitter_mgen_seq.awk
-    chmod +x analyze_latency_jitter_mgen_seg.awk
+[[ ! -d "$INSTALL_PATH" ]] && mkdir -p "$INSTALL_PATH"
 
-    cp mgen-simulator.sh $INSTALL_PATH/mgen-simulator
-    cp diffclock.sh $INSTALL_PATH/diffclock
-    cp clockdiff_client.py $INSTALL_PATH/clockdiff_client
-    cp clockdiff_server.py $INSTALL_PATH/clockdiff_server
-    cp analyze_latency_jitter_mgen.awk $INSTALL_PATH/analyze_latency_jitter_mgen
-    cp analyze_latency_jitter_mgen_seq.awk $INSTALL_PATH/analyze_latency_jitter_mgen_seq
-    cp analyze_latency_jitter_mgen_seg.awk $INSTALL_PATH/analyze_latency_jitter_mgen_seg
+chmod +x mgen-simulator.sh
+chmod +x diffclock.sh
+chmod +x clockdiff_client.py
+chmod +x clockdiff_server.py
+chmod +x analyze_latency_jitter_mgen.awk
+chmod +x analyze_latency_jitter_mgen_seq.awk
+chmod +x analyze_latency_jitter_mgen_seg.awk
+
+cp mgen-simulator.sh $INSTALL_PATH/mgen-simulator
+cp diffclock.sh $INSTALL_PATH/diffclock
+cp clockdiff_client.py $INSTALL_PATH/clockdiff_client
+cp clockdiff_server.py $INSTALL_PATH/clockdiff_server
+cp analyze_latency_jitter_mgen.awk $INSTALL_PATH/analyze_latency_jitter_mgen
+cp analyze_latency_jitter_mgen_seq.awk $INSTALL_PATH/analyze_latency_jitter_mgen_seq
+cp analyze_latency_jitter_mgen_seg.awk $INSTALL_PATH/analyze_latency_jitter_mgen_seg
